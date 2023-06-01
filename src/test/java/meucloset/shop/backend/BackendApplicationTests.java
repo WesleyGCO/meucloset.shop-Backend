069c0d62b863a0bea3bf67cc901bae23c4854538
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-05-31 21:29:31 -0300

    Início do projeto backend

---
+++ src/test/java/meucloset/shop/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package meucloset.shop.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
