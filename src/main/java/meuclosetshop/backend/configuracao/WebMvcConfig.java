45b244c37d317c9623cf8c47ea2bf1ab38776dc3
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-09-24 16:46:09 -0300

    Criado configuração

---
+++ src/main/java/meuclosetshop/backend/configuracao/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package meuclosetshop.backend.configuracao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/Frontend/**")
+                .addResourceLocations("classpath:/static/Frontend/");
+    }
+}
